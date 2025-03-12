e6b95b8c8c3901394299c8ceb460cdf23c42ea05
Shuler Hopkins <shulerhopkins7@gmail.com>
2025-03-12 10:02:23 -0500

    Spring 2025. Some issues fixed.

---
+++ Project3/SieveOfEratosthenes.py
@@ -0,0 +1,46 @@
+# Author:
+# Description: 
+
+from graphics import *
+
+# This function takes an integer n as input
+# Returns True if n is prime
+# Returns False if n is not prime
+def is_prime():
+    return 
+
+# add function description
+def draw_grid():
+    print()
+
+
+# add function description
+def draw_numbers():
+    print()
+
+# add function description
+def cover():
+    print()
+
+# add function description
+def cover_multiples_of(p):
+    print()
+
+
+
+def main():
+    # Draw grid of squares
+    # pause and wait for click
+    # Add numbers to squares
+    # pause and wait for click
+    # Loop over numbers
+    #   if number n is prime
+    #       cover up multiples of n that are greater than n and <= 300
+    #       pause and wait for click    
+    #   end of loop
+    # do something to let the user know the animation is over
+    # pause and wait for click
+    # close window
+
+
+main()
